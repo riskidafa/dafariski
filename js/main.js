e8e39aa133dfba76a1fcc05688e1a110ab1286c8
riskidafa <94170292+riskidafa@users.noreply.github.com>
2021-11-12 17:06:22 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const menuToggle = document.querySelector('.menu_toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
+})
+
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
